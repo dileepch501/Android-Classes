d0e2d33b8e339036a727b426c5dd1a015cfd8a49
dileep <dileep@sukshi.com>
2021-01-04 10:51:20 +0530

    android classes

---
+++ SecondProject/app/src/main/java/com/dileep/secondproject/SencondActivity.java
@@ -0,0 +1,14 @@
+package com.dileep.secondproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SencondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sencond);
+    }
+}
