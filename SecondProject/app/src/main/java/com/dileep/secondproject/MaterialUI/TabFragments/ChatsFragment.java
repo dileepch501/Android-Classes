f991f10d5bfc4ad724f05bf1fb2f8a2e1b065a79
dileep <dileep@sukshi.com>
2021-03-02 22:35:11 +0530

    1.Tablayout with FAB

--- SecondProject/app/src/main/java/com/dileep/secondproject/MaterialUI/TabFragments/ChatsFragment.java
+++ SecondProject/app/src/main/java/com/dileep/secondproject/MaterialUI/TabFragments/ChatsFragment.java
@@ -10,15 +10,33 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.dileep.secondproject.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ChatsFragment extends Fragment {
 
     View rootView;
+    FloatingActionButton floating_action_button;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView=inflater.inflate(R.layout.chats_tab,container,false);
+        floating_action_button=rootView.findViewById(R.id.floating_action_button);
+
+        floating_action_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             Snackbar.make(floating_action_button,"Heloooo", Snackbar.LENGTH_LONG)
+                     .setAction("Action", new View.OnClickListener() {
+                         @Override
+                         public void onClick(View view) {
+
+                         }
+                     }).show();
+            }
+        });
 
         return rootView;
     }
--- SecondProject/app/src/main/java/com/dileep/secondproject/MaterialUI/TabFragments/StatusFragment.java
+++ SecondProject/app/src/main/java/com/dileep/secondproject/MaterialUI/TabFragments/StatusFragment.java
@@ -4,20 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.dileep.secondproject.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class StatusFragment  extends Fragment {
     View rootView;
+    FloatingActionButton floatingActionButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         rootView=inflater.inflate(R.layout.status_tab,container,false);
+
+        floatingActionButton=rootView.findViewById(R.id.floating_action_button);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(floatingActionButton,"This is snakbar",Snackbar.LENGTH_LONG)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(getContext(), "clicked undo", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+
         return rootView;
     }
 }
