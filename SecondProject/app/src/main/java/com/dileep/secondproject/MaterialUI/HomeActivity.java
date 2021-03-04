185eccbb001b8b50ceeb2183097e926bfc462094
dileep <dileep@sukshi.com>
2021-03-04 23:07:45 +0530

    1.NavigationDrawer

--- SecondProject/app/src/main/java/com/dileep/secondproject/MaterialUI/HomeActivity.java
+++ SecondProject/app/src/main/java/com/dileep/secondproject/MaterialUI/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.dileep.secondproject.MaterialUI;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,6 +13,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,16 +26,19 @@ import com.dileep.secondproject.MaterialUI.TabFragments.ChatsFragment;
 import com.dileep.secondproject.MaterialUI.TabFragments.PagerAdapter1;
 import com.dileep.secondproject.MaterialUI.TabFragments.StatusFragment;
 import com.dileep.secondproject.R;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
 
     Toolbar toolbar;
     Button popBtn;
     TabLayout tabLayout;
     ViewPager viewPager1;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +48,10 @@ public class HomeActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         tabLayout=findViewById(R.id.tablayout);
         viewPager1=findViewById(R.id.viewPager1);
+        drawerLayout=findViewById(R.id.drawerLayout);
+        navigationView=findViewById(R.id.navigationView);
 //        popBtn=findViewById(R.id.popBtn);
-
+        navigationView.setNavigationItemSelectedListener(this);
 
         setSupportActionBar(toolbar);
 
@@ -51,6 +59,19 @@ public class HomeActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("Chats"));
         tabLayout.addTab(tabLayout.newTab().setText("Status"));
         tabLayout.addTab(tabLayout.newTab().setText("Calls"));
+
+
+        /*ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();*/
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(Gravity.RIGHT);
+            }
+        });
+
+
 
 //        replaceFragment(new ChatsFragment());
 
@@ -142,7 +163,8 @@ public class HomeActivity extends AppCompatActivity {
 
 
     }
-    private void replaceFragment(Fragment fragment) {
+
+/*    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
@@ -150,7 +172,7 @@ public class HomeActivity extends AppCompatActivity {
         transaction.replace(R.id.fragment_container, fragment);
 
         transaction.commit();
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -184,5 +206,31 @@ public class HomeActivity extends AppCompatActivity {
         Intent signupIntent = new Intent(HomeActivity.this, MaterialUIExample.class);
         startActivity(signupIntent);
         finish();
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.chats:
+                drawerLayout.closeDrawers();
+                navigationView.getMenu().getItem(0).setChecked(true);
+                viewPager1.setCurrentItem(0);
+                return true;
+            case R.id.status:
+                drawerLayout.closeDrawers();
+                navigationView.getMenu().getItem(1).setChecked(true);
+                viewPager1.setCurrentItem(1);
+                return true;
+            case R.id.calls:
+                drawerLayout.closeDrawers();
+                navigationView.getMenu().getItem(2).setChecked(true);
+                viewPager1.setCurrentItem(2);
+                return true;
+            default:
+                return false;
+        }
+
     }
 }
