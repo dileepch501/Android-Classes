a51a68f424bb14673bd748e3632e9fe7f19d7350
dileep <dileep@sukshi.com>
2021-03-15 22:47:59 +0530

    1.Firebase data base

--- SecondProject/app/src/main/java/com/dileep/secondproject/MaterialUI/TabFragments/ChatsFragment.java
+++ SecondProject/app/src/main/java/com/dileep/secondproject/MaterialUI/TabFragments/ChatsFragment.java
@@ -1,6 +1,8 @@
 package com.dileep.secondproject.MaterialUI.TabFragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +14,18 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.dileep.secondproject.MaterialUI.HomeActivity;
 import com.dileep.secondproject.MaterialUI.TabAdapters.ChatsListAdapter;
+import com.dileep.secondproject.Pjoclasses.UsersPojo;
 import com.dileep.secondproject.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChatsFragment extends Fragment {
 
@@ -39,16 +48,60 @@ public class ChatsFragment extends Fragment {
         floating_action_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-             Snackbar.make(floating_action_button,"Heloooo", Snackbar.LENGTH_LONG)
-                     .setAction("Action", new View.OnClickListener() {
-                         @Override
-                         public void onClick(View view) {
 
-                         }
-                     }).show();
+//                Intent intent=new Intent(getContext(), HomeActivity.class);
+//                startActivity(intent);
+                storeData();
+
+
             }
         });
 
         return rootView;
+    }
+
+    private void storeData() {
+
+        UsersPojo usersPojo=new UsersPojo();
+        usersPojo.setName("Dileep");
+        usersPojo.setMessage("How are you..");
+        usersPojo.setCount("3");
+        usersPojo.setTime("10:30 pm");
+
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("usersTable");
+
+        myRef.push().setValue(usersPojo);
+//        myRef.setValue(usersPojo);
+
+
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Snackbar.make(floating_action_button,value, Snackbar.LENGTH_LONG)
+//                        .setAction("Action", new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//
+//                            }
+//                        }).show();
+////                Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+////                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+
+
+
+
     }
 }
---
+++ SecondProject/app/src/main/java/com/dileep/secondproject/Pjoclasses/UsersPojo.java
@@ -0,0 +1,40 @@
+package com.dileep.secondproject.Pjoclasses;
+
+public class UsersPojo {
+    String name,message,time,count;
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+}
