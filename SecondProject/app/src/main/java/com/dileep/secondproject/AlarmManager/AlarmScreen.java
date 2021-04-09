7278d70beecaf2dcad21d3b0f54d7ccaac07afe0
dileep <dileep@sukshi.com>
2021-04-09 22:48:39 +0530

    1.Notifications

--- SecondProject/app/src/main/java/com/dileep/secondproject/AlarmManager/AlarmScreen.java
+++ SecondProject/app/src/main/java/com/dileep/secondproject/AlarmManager/AlarmScreen.java
@@ -1,17 +1,23 @@
 package com.dileep.secondproject.AlarmManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.dileep.secondproject.MaterialUI.HomeActivity;
 import com.dileep.secondproject.R;
 
 import java.util.Calendar;
@@ -20,18 +26,42 @@ public class AlarmScreen extends AppCompatActivity {
 
     EditText timesec;
     Button start;
+    public static String CHANNEL_ID="channel_1";
+    public static String CHANNEL_NAME="Alarm notification";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_screen);
         timesec=findViewById(R.id.timesec);
         start=findViewById(R.id.start);
+        createNotificationChannel();
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int time=Integer.parseInt(timesec.getText().toString());
+                setAlarm(time);
+                // Create an explicit intent for an Activity in your app
+                /*Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
 
-                setAlarm(10);
+                Intent intent2 = new Intent(getApplicationContext(), AlarmScreen.class);
+                PendingIntent pendingIntent2 = PendingIntent.getActivity(getApplicationContext(), 0, intent2, 0);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("My notification")
+                        .setContentText("This is testing Notification")
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText("This is testing Notification..."))
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setContentIntent(pendingIntent)
+                        .addAction(R.drawable.notification_icon,"Open",pendingIntent2);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+// notificationId is a unique int for each notification that you must define
+                notificationManager.notify(1, builder.build());*/
             }
         });
 
@@ -51,5 +81,19 @@ public class AlarmScreen extends AppCompatActivity {
         alarmManager.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+(time*1000), pendingIntent1);
 
 
+    }
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
+            channel.setDescription("This is Alarm Notifications");
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
--- SecondProject/app/src/main/java/com/dileep/secondproject/AlarmManager/BroadCastRecieverCustome.java
+++ SecondProject/app/src/main/java/com/dileep/secondproject/AlarmManager/BroadCastRecieverCustome.java
@@ -1,14 +1,43 @@
 package com.dileep.secondproject.AlarmManager;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.dileep.secondproject.MaterialUI.HomeActivity;
+import com.dileep.secondproject.R;
+
+import static com.dileep.secondproject.AlarmManager.AlarmScreen.CHANNEL_ID;
+
 public class BroadCastRecieverCustome extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Toast.makeText(context, "Alaram triggered", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(context, "Alaram triggered", Toast.LENGTH_SHORT).show();
+        Intent intent1 = new Intent(context, HomeActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, 0);
+
+        Intent intent2 = new Intent(context, AlarmScreen.class);
+        PendingIntent pendingIntent2 = PendingIntent.getActivity(context, 0, intent2, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("My notification")
+                .setContentText("This is testing Notification")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("This is testing Notification..."))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .addAction(R.drawable.notification_icon,"Open",pendingIntent2);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, builder.build());
     }
 }
